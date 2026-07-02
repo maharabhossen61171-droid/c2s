773e112a26b3d22cc69e31ae06d1b804109ba1ff
maharabhossen61171-droid <maharabhossen61171@gmail.com>
2026-07-02 12:04:39 +0300

    Add files via upload

---
+++ apps/api/src/main.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { RequestIdInterceptor } from './common/interceptors/request-id.interceptor';
+import { ResponseTransformInterceptor } from './common/interceptors/response-transform.interceptor';
+import { AuditLogInterceptor } from './common/interceptors/audit-log.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(
+    new RequestIdInterceptor(),
+    new ResponseTransformInterceptor(),
+    new AuditLogInterceptor(),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('C2S API')
+    .setDescription('Content Creator to Sponsor API documentation')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
